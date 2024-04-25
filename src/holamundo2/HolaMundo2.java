9d68ae9e9d7d360be7ca4131dbebf43c9b25a086
daniel <daniel.gutsan.4@educa.jcyl.es>
2024-04-25 17:18:21 +0200

    Ultima Versión

--- src/holamundo2/HolaMundo2.java
+++ src/holamundo2/HolaMundo2.java
@@ -14,7 +14,7 @@ public class HolaMundo2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola ");
          System.out.println("Hola");
     }
     
